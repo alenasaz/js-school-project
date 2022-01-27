a65ef8302aaf445a3eab686c2487308a4733a12d
alenasaz <aLeNka_saZaNova@mail.ru>
2022-01-27 14:14:04 +0300

    Added Github login test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import './App.css';
-import AuthenticationForm from './Authentication/AuthenticationForm';
+import React, { createContext, useReducer } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginPage from "./components/LoginPage/LoginPage";
+import HomePage from "./components/HomePage/HomePage";
+import { initialState, reducer } from "./store/reducer";
+import "./App.css";
+
+export const AuthContext = createContext();
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
-    <div className="App">
-    <AuthenticationForm/>
-    </div>
+    <AuthContext.Provider
+      value={{
+        state,
+        dispatch,
+      }}
+    >
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/" element={<HomePage />} />
+          </Routes>
+        </div>
+      </Router>
+    </AuthContext.Provider>
   );
 }
-
 
 export default App;
