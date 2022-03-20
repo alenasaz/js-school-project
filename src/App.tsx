7f33339950e93915892d4d1987be482745f343a5
Dima <dpolevodin@gmail.com>
2022-03-21 02:03:44 +0500

    added apollo client to gql + Hasura

--- src/App.tsx
+++ src/App.tsx
@@ -4,41 +4,15 @@ import { Routes, Route } from 'react-router-dom'
 import { AdminLoginPage } from './components/AdminLoginPage/AdminLoginPage'
 import { AdminPage } from './components/AdminPage/AdminPage'
 import StudentPage from './components/StudentPage/StudentPage'
-import { ApolloClient, gql, InMemoryCache, useQuery } from '@apollo/client'
-
-export const client = new ApolloClient({
-  uri: 'https://js-school-statement.hasura.app/v1/graphql',
-  cache: new InMemoryCache(),
-  headers: {
-    'x-hasura-access-key': `ep7YkQEoJCY2fYGhIiyZGiJBRr4at9RL8UQdE4KFUSQYWFnYWdZtX2v7UlAOWKQq`,
-  },
-})
-
-const USERS_NAME = gql`
-  query GetUsersName {
-    users {
-      name
-    }
-  }
-`
-
-function ExchangeUsers() {
-  const { loading, error, data } = useQuery(USERS_NAME)
-
-  if (loading) return <p>Loading...</p>
-  if (error) return <p>Error :(</p>
-
-  return data.users.map((item: { name: string }) => (
-    <div>
-      <p>имя: {item?.name}</p>
-    </div>
-  ))
-}
+import { GetUserRoleByName } from './gql/index'
 
 const App = () => {
+  //TODO: удалить, тестовые данные для Hasura
+  const test = GetUserRoleByName('admin')
+  console.log(test)
+
   return (
     <div className="App">
-      <ExchangeUsers />
       <Routes>
         <Route path="/" element={<div>Ведомость школы JS</div>} />
         <Route path="/login" element={<div>login</div>} />
--- src/index.tsx
+++ src/index.tsx
@@ -2,39 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  useQuery,
-  gql,
-} from '@apollo/client'
-
-export const client = new ApolloClient({
-  uri: 'https://js-school-statement.hasura.app/v1/graphql',
-  cache: new InMemoryCache(),
-})
-
-const USERS_NAME = gql`
-  query GetUsersName {
-    users {
-      name
-    }
-  }
-`
-
-function ExchangeUsers() {
-  const { loading, error, data } = useQuery(USERS_NAME)
-
-  if (loading) return <p>Loading...</p>
-  if (error) return <p>Error :(</p>
-
-  return data.users.map((item: { name: string }) => (
-    <div>
-      <p>имя: {item?.name}</p>
-    </div>
-  ))
-}
+import { ApolloProvider } from '@apollo/client'
+import { client } from './gql/index'
 
 ReactDOM.render(
   <ApolloProvider client={client}>
