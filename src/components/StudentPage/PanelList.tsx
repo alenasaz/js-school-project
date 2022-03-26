797e791745a957a54b124f77bbc39d5fbd4321e7
alenasaz <aLeNka_saZaNova@mail.ru>
2022-03-26 20:29:17 +0300

    updated things

--- src/components/StudentPage/PanelList.tsx
+++ src/components/StudentPage/PanelList.tsx
@@ -1,30 +1,26 @@
-import { Collapse } from 'antd';
-import { StudentRepository } from './interfaces';
+import { Collapse } from 'antd'
+import { StudentRepository } from './interfaces'
 
-const { Panel } = Collapse;
+const { Panel } = Collapse
 
-export const PanelList = (prop: {repositories: StudentRepository[]}) => {
-
-    return  (
-        <>
-           {
-        prop.repositories.map((item: StudentRepository) => (
-<Collapse defaultActiveKey={[]} >
-        <Panel header={item?.name} key={`${item.id}`}>
-           
-          <p>URL репозитория:</p>
-          <p>
-            <a href={item.html_url ?? ''}>{item.full_name ?? ''}</a>
-          </p>
-          <p>Описание: {item.description|| "осутствует"}</p>
-          <p>Дата создания: {(item.created_at).slice(0,10).split("-").reverse().join(".")}</p>
-         
-        </Panel>
+export const PanelList = (prop: { repositories: StudentRepository[] }) => {
+  return (
+    <div>
+      {prop.repositories.map((item: StudentRepository) => (
+        <Collapse defaultActiveKey={[]}>
+          <Panel header={item?.name} key={`${item.id}`}>
+            <p>URL репозитория:</p>
+            <p>
+              <a href={item.html_url ?? ''}>{item.full_name ?? ''}</a>
+            </p>
+            <p>Описание: {item.description || 'осутствует'}</p>
+            <p>
+              Дата создания:{' '}
+              {item.created_at.slice(0, 10).split('-').reverse().join('.')}
+            </p>
+          </Panel>
         </Collapse>
-        )
-    )
-    }
-    </>
-    )
-   }
-    
+      ))}
+    </div>
+  )
+}
--- src/components/StudentPage/RepositoriesList.tsx
+++ src/components/StudentPage/RepositoriesList.tsx
@@ -1,44 +1,40 @@
-import { useState, useEffect } from 'react';
-import { Collapse,Button,Space } from 'antd';
-import { Urls } from './constants';
-import {PanelList} from './PanelList';
+import { useState, useEffect } from 'react'
+import { Collapse, Button, Space } from 'antd'
+import { Urls } from './constants'
+import { PanelList } from './PanelList'
 
 export const RepositoriesList = (prop: { user: string }) => {
   //TODO: Перенести в эффектор. Сделать связку со store User,
   // Запрос возвращается список указанного количества репозиториев (repositoriesPerPage)
   // по нику gitHub в порядке убывания по дате создания
-  const [allRepoShown, setAllRepoShown, ] = useState(false);
-  const [repositories, setRepositories] = useState([]);
-  const [repositoriesPerPage, setRepositoriesPerPage] = useState(10);
-  const repositoriesUrl = `${Urls.rootUsersUrl}${prop.user}/repos?per_page=${repositoriesPerPage}&sort=order`;
+  const [allRepoShown, setAllRepoShown] = useState(false)
+  const [repositories, setRepositories] = useState([])
+  const [repositoriesPerPage, setRepositoriesPerPage] = useState(10)
+  const repositoriesUrl = `${Urls.rootUsersUrl}${prop.user}/repos?per_page=${repositoriesPerPage}&sort=order`
 
   useEffect(() => {
     fetch(repositoriesUrl)
       .then((data) => data.json())
       .then((result) => {
-        setRepositories(result);
-      });
-  }, [repositoriesUrl]);
+        setRepositories(result)
+      })
+      .catch((err) => console.log(err))
+  }, [repositoriesUrl])
 
   const handleClick = () => {
-    return (
-      allRepoShown ?
-        (setAllRepoShown(false), setRepositoriesPerPage(10))
-        :
-        (setAllRepoShown(true), setRepositoriesPerPage(100))
-    )
+    return allRepoShown
+      ? (setAllRepoShown(false), setRepositoriesPerPage(10))
+      : (setAllRepoShown(true), setRepositoriesPerPage(100))
   }
-console.log('репо', repositories);
 
   return (
     <>
-      
-        <PanelList repositories={repositories}/>
-    
+      <PanelList repositories={repositories} />
+
       <Button type="default" onClick={handleClick}>
-        {allRepoShown ? "Скрыть " : "Показать "}
+        {allRepoShown ? 'Скрыть ' : 'Показать '}
         весь список репозиториев
       </Button>
     </>
-  );
+  )
 }
