b84d95f4850a4e9ae5fc12ced82bc17212b3541c
Dima <dpolevodin@gmail.com>
2022-03-27 11:39:23 +0500

    Revert "24 hasura add hasura to project"

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,8 @@ import { Routes, Route } from 'react-router-dom'
 import { AdminLoginPage } from './components/AdminLoginPage/AdminLoginPage'
 import { AdminPage } from './components/AdminPage/AdminPage'
 import StudentPage from './components/StudentPage/StudentPage'
-import { GetUserNameById } from './gql/index'
 
 const App = () => {
-  //TODO: удалить, тестовые данные для Hasura
-  const hasuraTestQuery = GetUserNameById(77278863)
-  console.log(hasuraTestQuery)
-
   return (
     <div className="App">
       <Routes>
@@ -24,7 +19,7 @@ const App = () => {
             </div>
           }
         />
-        <Route path="/student" element={<StudentPage />} />
+        <Route path="/student" element={<StudentPage/>}/>
         <Route
           path="/admin"
           element={
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import { BrowserRouter } from 'react-router-dom'
-import { ApolloProvider } from '@apollo/client'
-import { client } from './gql/index'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { BrowserRouter } from "react-router-dom";
+
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <React.StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </React.StrictMode>
-  </ApolloProvider>,
-  document.getElementById('root'),
-)
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
