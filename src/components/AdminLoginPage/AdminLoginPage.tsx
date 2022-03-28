0aa9ebee928e1e6b67306433b272c162eaf1d370
Dima <dpolevodin@gmail.com>
2022-03-28 14:40:49 +0500

    delete unused deps

--- src/components/AdminLoginPage/AdminLoginPage.tsx
+++ src/components/AdminLoginPage/AdminLoginPage.tsx
@@ -1,5 +1,4 @@
 import { useNavigate } from 'react-router-dom'
-import { useEffect, useState } from 'react'
 import { useStore } from 'effector-react'
 import { checkIsAdmin } from 'src/utils/checkIsAdmin'
 import { Form, Input, Button, Alert, Space } from 'antd'
