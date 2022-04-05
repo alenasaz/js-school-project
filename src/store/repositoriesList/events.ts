2544507e61d03b2eb3fdffe6b7ea260bcbe15ce8
alenasaz <aLeNka_saZaNova@mail.ru>
2022-04-05 12:19:21 +0300

    Changed type of event

--- src/store/repositoriesList/events.ts
+++ src/store/repositoriesList/events.ts
@@ -2,5 +2,5 @@ import { domain } from '../domain'
 
 export const setAllRepoVisibleEvent = domain.createEvent()
 export const setAllRepoUnvisibleEvent = domain.createEvent()
-export const setRepoCountEvent = domain.createEvent()
+export const setRepoCountEvent = domain.createEvent<number>()
 export const setRepoListEvent = domain.createEvent()
